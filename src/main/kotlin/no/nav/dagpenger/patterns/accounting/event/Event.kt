0c52c32efc3d4d2b5ea11cb710b0567ffc0184a6
Mona Kjeldsrud <Mona.Kjeldsrud.nav.no>
2022-12-07 14:43:01 +0100

    Lagt til Sale event Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- src/main/kotlin/no/nav/dagpenger/patterns/accounting/event/Event.kt
+++ src/main/kotlin/no/nav/dagpenger/patterns/accounting/event/Event.kt
@@ -1,18 +1,36 @@
 package no.nav.dagpenger.patterns.accounting.event
 
 import no.nav.dagpenger.patterns.accounting.AccountNumber
+import no.nav.dagpenger.patterns.accounting.Money
+import no.nav.dagpenger.patterns.accounting.Vendor
 import java.time.LocalDateTime
 
-abstract class Event(
-    private val eventType: EventType,
-    private val accountNumber: AccountNumber,
-    private val whenOccured: LocalDateTime,
-    private val whenNoticed: LocalDateTime
-) {
-    fun eventType() = eventType
-    fun accountNumber() = accountNumber
-    fun whenOccured() = whenOccured
-    fun whenNoticed() = whenNoticed
+interface Event {
+
+    fun newEvent(
+        type: EventType,
+        account: AccountNumber,
+        whenOccurred: LocalDateTime,
+        whenNoticed: LocalDateTime
+    ): Event
+
+    fun eventType(): EventType
+    fun accountNumber(): AccountNumber
+    fun whenOccured(): LocalDateTime
+    fun whenNoticed(): LocalDateTime
+}
+
+interface Sale : Event {
+    fun newSale(
+        account: AccountNumber,
+        whenOccurred: LocalDateTime,
+        whenNoticed: LocalDateTime,
+        vendor: Vendor,
+        amount: Money
+    ): Sale
+
+    fun vendor(): Vendor
+    fun getAmount(): Money
 }
 
 enum class EventType {
