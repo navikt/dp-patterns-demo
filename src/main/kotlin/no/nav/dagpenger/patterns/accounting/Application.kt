e7f4ff8b2611804ceb4b58accfcf1d083bec9a62
Mona Kjeldsrud <Mona.Kjeldsrud.nav.no>
2022-12-09 11:03:05 +0100

    Fikset på Application.kt og lagt på innhold i TemporalCollection

--- src/main/kotlin/no/nav/dagpenger/patterns/accounting/Application.kt
+++ src/main/kotlin/no/nav/dagpenger/patterns/accounting/Application.kt
@@ -5,20 +5,23 @@ import no.nav.dagpenger.patterns.accounting.postering.MultipliserMedSatsPR
 import no.nav.dagpenger.patterns.accounting.postering.PosteringType
 import java.time.LocalDateTime
 
-class Application {
-    fun main() {
-        settStandardforbruk("Tonje")
-    }
 
-    private fun settStandardforbruk(navn: String) {
-        val kunde = Kunde(navn)
-        val standardAvtale = TjenesteAvtale()
-        standardAvtale.setSats(10.0)
-        standardAvtale.leggTilPosteringsregel(
-            hendelseType = HendelseType.forbruk,
-            posteringsregel = MultipliserMedSatsPR(PosteringType.grunnforbruk),
-            LocalDateTime.now()
-        )
-        kunde.leggTil(standardAvtale)
-    }
+fun main() {
+    settStandardforbruk("Tonje")
+}
+
+private fun settStandardforbruk(navn: String) {
+    val kunde = Kunde(navn)
+    val standardAvtale = TjenesteAvtale()
+    val dato = LocalDateTime.now()
+    standardAvtale.setSats(10.0)
+    standardAvtale.leggTilPosteringsregel(
+        hendelseType = HendelseType.forbruk,
+        posteringsregel = MultipliserMedSatsPR(PosteringType.grunnforbruk),
+        dato
+    )
+    kunde.leggTil(standardAvtale)
+    println("Kundens navn er $navn")
+    println("Kunden har en avtale med sats ${standardAvtale.sats()}")
+    //TODO println("Posteringsregel er ${standardAvtale.posteringsregel(dato,HendelseType.forbruk).toString()}")
 }
--- src/main/kotlin/no/nav/dagpenger/patterns/accounting/TemporalCollection.kt
+++ src/main/kotlin/no/nav/dagpenger/patterns/accounting/TemporalCollection.kt
@@ -3,11 +3,15 @@ package no.nav.dagpenger.patterns.accounting
 import no.nav.dagpenger.patterns.accounting.postering.MultipliserMedSatsPR
 import no.nav.dagpenger.patterns.accounting.postering.PosteringType
 import no.nav.dagpenger.patterns.accounting.postering.Posteringsregel
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 class TemporalCollection {
+
+    private val innhold = mutableMapOf<LocalDateTime, Posteringsregel>()
+
     fun put(gyldigFra: LocalDateTime, posteringsregel: Posteringsregel) {
-        TODO("Not yet implemented")
+        innhold[gyldigFra] = posteringsregel
     }
 
     fun get(forDato: LocalDateTime) : Posteringsregel {
